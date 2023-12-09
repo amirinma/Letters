d9a1f17f676d0b4bb3511cb2d49c565010307f76
ziaro <zia.ro@live.com>
2023-12-09 12:12:39 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args){
+        char letter;
+        letter = 'A';
+        System.out.println(letter);
+        letter = 'B';
+        System.out.println(letter);
+        String example = "This is coming from a char litteral.";
+        System.out.println(example);
+    }
+}
